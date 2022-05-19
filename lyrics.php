61216104dfc399b2efabfaa2fdf01539abc72f49
BalagwisJohnDenver <imba_jhey@yahoo.com>
2022-05-19 17:54:55 +0800

    change the link

--- lyrics.php
+++ lyrics.php
@@ -94,7 +94,7 @@
                 <h3 class="text-center float-md-start mb-0">Song Lyrics App</h3>
                 <nav class="nav nav-masthead justify-content-center float-md-end">
                     <a class="nav-link fw-bold py-1 px-0 " href="#" onclick="history.back()">Home</a>
-                    <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="index.php">Top Lyrics</a>
+                    <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="lyrics.php">Top Lyrics</a>
                     <a class="nav-link fw-bold py-1 px-0" href="#">Sign In</a>
                 </nav>
             </div>
