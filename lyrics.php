ed571f5471931d1bcf0b91805581d6118f0d4ccd
BalagwisJohnDenver <imba_jhey@yahoo.com>
2022-05-19 18:04:46 +0800

    change Sign In to Sign in

--- lyrics.php
+++ lyrics.php
@@ -95,7 +95,7 @@
                 <nav class="nav nav-masthead justify-content-center float-md-end">
                     <a class="nav-link fw-bold py-1 px-0 " href="index.php" onclick="history.back()">Home</a>
                     <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="lyrics.php">Top Lyrics</a>
-                    <a class="nav-link fw-bold py-1 px-0" href="signin.php">Sign In</a>
+                    <a class="nav-link fw-bold py-1 px-0" href="signin.php">Sign in</a>
                 </nav>
             </div>
         </header>
