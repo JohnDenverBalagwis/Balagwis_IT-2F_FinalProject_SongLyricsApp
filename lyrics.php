6df215d250b3127c425893ec5aa456fe6682a663
BalagwisJohnDenver <imba_jhey@yahoo.com>
2022-05-22 10:24:57 +0800

    Updated the app name

--- lyrics.php
+++ lyrics.php
@@ -91,7 +91,7 @@
     <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
         <header class="mb-auto p-3">
             <div>
-                <h3 class="text-center float-md-start mb-0">Song Lyrics App</h3>
+                <h3 class="text-center float-md-start mb-0">Lyrixmatch</h3>
                 <nav class="nav nav-masthead justify-content-center float-md-end">
                     <a class="nav-link fw-bold py-1 px-0 " href="index.php" onclick="history.back()">Home</a>
                     <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="lyrics.php">Top Lyrics</a>
